5f798e1ad2a9eedb459a68278ae86fffcf3e6d97
Stoil Stoichev <stoilstoychev@gmail.com>
2016-08-15 11:21:54 +0300

    added aliens and stuff

---
+++ Assets/AlienBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class AlienBehaviour : MonoBehaviour
+{
+
+    public GameObject moveTarget;
+    public GameObject fireTarget;
+
+    public int health = 100;
+    public int firerate = 10; //1 to 10;
+    public int power = 10;//1 to 10;
+    public int speed = 10;//1 to 10;
+
+
+    protected void Move()
+    {
+        var target = moveTarget.transform.position;
+        if (Vector3.Distance(transform.position, target) > 0.3f)
+        {
+
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+            var dir = target - transform.position;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    protected abstract void Attack();
+
+    
+}
+
