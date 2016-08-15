bce0234e5c9c0ca8c34edc5de839c0850cde5de7
Martin Doychev <martin.doychev@abv.bg>
2016-08-15 11:27:02 +0300

    replaced deprecated project with new version (simple network manager); project is 3d

---
+++ Assets/AlienController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienController : MonoBehaviour {
+
+    private GameObject mainBase;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        mainBase = GameObject.Find("MainBase");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, mainBase.transform.position, step);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log(coll);
+        if (coll.gameObject.name == "MainBase")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
