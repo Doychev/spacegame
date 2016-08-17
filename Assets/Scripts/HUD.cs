3fd526f1ca212cf9c8ddcfe94257179c66953834
Stoil Stoichev <stoilstoychev@gmail.com>
2016-08-17 03:50:19 +0300

    player hud

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class HUD : NetworkBehaviour
+{
+    [SyncVar(hook = "SetupHUDp1")]
+    public string p1Info = "p1 info placeholder";
+
+    [SyncVar(hook = "SetupHUDp2")]
+    public string p2Info = "p2 info placeholder";
+
+    public void SetupHUDp1(string info)
+    {
+        var playersUI = transform.GetComponentsInChildren<Text>();
+        playersUI[0].text = info;
+    }
+
+    public void SetupHUDp2(string info)
+    {
+        var playersUI = transform.GetComponentsInChildren<Text>();
+        playersUI[1].text = info;
+    }
+}
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -11,6 +11,9 @@ public class Manager : NetworkManager
     private GameObject HUD;
     private List<NetworkConnection> players = new List<NetworkConnection>();
 
+    public NetworkConnection defender = null;
+    public NetworkConnection attacker = null;
+
     void Awake()
     {
         this.matchSize = 2;
@@ -28,36 +31,73 @@ public class Manager : NetworkManager
 
     }
 
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        Debug.Log("OnPlayerConnected");
-    }
-
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        Debug.Log("host " + client.connection.connectionId);
-        Debug.Log("A client has connected " + conn.connectionId);
-    }
-
     public override void OnServerReady(NetworkConnection conn)
     {
-        Debug.Log("a players is ready");
+        print("A players is ready...");
         players.Add(conn);
 
         if (players.Count >= 2)
         {
+            print("A game begins...");
+            SetOpposingSides();
             SetupHUD();
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        print("Disconnected connection id " + conn.connectionId);
+        players.Remove(conn);
+        EmptyHUD();
+    }
+
+    public override void OnStopHost()
+    {
+        print("Host has been stopped...");
+        if (players.Count >= 1)
+        {
+            EmptyHUD();
+            players.RemoveAt(0);
         }
     }
 
     private void SetupHUD()
     {
-
         var HUD = GameObject.Find("HUD");
-        HUD.GetComponent<HUD>().SetupHUD(players);
-       
-        //var players = HUD.GetComponentsInChildren<Text>();
-        //players[0].text = "Player 1:" + this.players[0].connectionId;
-        //players[1].text = "Player 2:" + this.players[1].connectionId;
+        var hudScript = HUD.GetComponent<HUD>();
+
+        var p1Role = defender.connectionId == players[0].connectionId ? "Defender" : "Attacker";
+        var p2Role = defender.connectionId == players[1].connectionId ? "Defender" : "Attacker";
+
+        var p1Info = "P1 (Hosting)(" + p1Role + ")";
+        var p2Info = "P2 (Joined)(" + p2Role + ")";
+
+        hudScript.p1Info = p1Info;
+        hudScript.p2Info = p2Info;
     }
+
+    private void EmptyHUD()
+    {
+        var HUD = GameObject.Find("HUD");
+        var hudScript = HUD.GetComponent<HUD>();
+        hudScript.p1Info = "P1";
+        hudScript.p2Info = "P2";
+    }
+
+    private void SetOpposingSides()
+    {
+        //This can be random but for now let's keep it non-random for easy testing
+        //Change DrawDefender to make it truly random
+        var defenderIndex = DrawDefender();
+        var attackerIndex = (defenderIndex + 1) % 2;
+
+        defender = players[defenderIndex];
+        attacker = players[attackerIndex];
+    }
+
+    private int DrawDefender()
+    {
+        return Random.Range(0, 1);
+    }
+
 }
