e07560040337b118299782ef134ef84ea2fe4466
Stoil Stoichev <stoilstoychev@gmail.com>
2016-09-11 16:08:45 -0400

    healh bar decrease and changes color

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -4,23 +4,31 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class AlienBehaviour : NetworkBehaviour
 {
     public GameObject moveTarget;
     public GameObject fireTarget;
 
-    [SyncVar]
+    [SyncVar(hook = "DecreaseHealthBar")]
     public int health = 100;
+
+    private int maxHealth = 100;
 
     public int firerate = 10; //1 to 10;
     public int power = 10;//1 to 10;
     public int speed = 10;//1 to 10;
 
+    private Image healthImage;
+
     public void Start()
     {
         if (moveTarget == null)
             moveTarget = GameObject.Find("HumanBase");
+
+        healthImage = GetComponentInChildren<Image>();
+        maxHealth = health;
     }
 
     public void Update()
@@ -54,6 +62,13 @@ public class AlienBehaviour : NetworkBehaviour
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    public void DecreaseHealthBar(int currentHealthNumber)
+    {
+        var fill = currentHealthNumber / (float)maxHealth;
+        healthImage.fillAmount = fill;
+        healthImage.color = Color.Lerp(Color.red, Color.green, fill);
     }
 }
 
